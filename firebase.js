4dc8f5cc620907131dd22a189790309751aea72a
YahyaAhmad-hub <yahyaahmad@gmail.com>
2024-12-15 11:41:33 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyALqZWom18A6s1pQ5tbSRTjQQ5nlaA0UpI",
+  authDomain: "jzamma.firebaseapp.com",
+  databaseURL: "https://jzamma-default-rtdb.firebaseio.com",
+  projectId: "jzamma",
+  storageBucket: "jzamma.firebasestorage.app",
+  messagingSenderId: "381531227123",
+  appId: "1:381531227123:web:0917825f587d0e90f0c886",
+  measurementId: "G-41WQ8BFYTC"
+};
